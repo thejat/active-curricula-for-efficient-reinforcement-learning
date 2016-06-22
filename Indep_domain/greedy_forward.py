39cfe3895b7e3509fd79a198cfe47b30c2ac1d32
Vikas Jain <vikasj@cse.iitk.ac.in>
2016-06-22 13:57:17 +0530

    restructuring directory files

---
+++ Indep_domain/greedy_forward.py
@@ -0,0 +1,139 @@
+import numpy as np
+from maze import Maze 
+import copy
+import random
+import matplotlib.pyplot as plt
+
+def change(Q1, Q2, env):
+	thres = 0.0 
+	for i in env.free_cells:
+		prev_val = sum(Q1[i[0]][i[1]])
+		new_val = sum(Q2[i[0]][i[1]])
+		# print (prev_val, new_val)
+		if(abs(prev_val - new_val) > thres):
+			change = 1
+			break
+		else:
+			change = 0
+	return change
+
+def learnTask(task_num, Q, task, epsilon = 0.3, alpha = 1, discount = 0.9):
+	grid_size = len(Q)
+	env = Maze(grid_size, task[:-1], task[-1])
+	env.draw("greedy_max/tasks/", task_num)
+	num_actions = env.num_actions
+
+	## Learning source task
+	step = 0
+	episode = 0
+	exceed = 0
+	not_change_count = 0
+	change_no = 5
+	while True:
+		env.reset()
+		game_over = False
+		max_iter = 100
+		itr = 0
+		episode += 1
+		Q2 = copy.deepcopy(Q)
+		while not (game_over or itr > max_iter):
+			itr += 1
+			curr_state = env.state()
+			if np.random.rand() <= epsilon:
+				action = np.random.randint(0, num_actions)
+			else:
+				action = np.argmax(Q[curr_state[0]][curr_state[1]])
+			next_state, reward, game_over = env.act(action)
+			step += 1
+			# Q-learning update
+			Q[curr_state[0]][curr_state[1]][action] = Q[curr_state[0]][curr_state[1]][action] + alpha*(reward + discount*max(Q[next_state[0]][next_state[1]]) - Q[curr_state[0]][curr_state[1]][action])
+		if (itr > max_iter):
+			exceed += 1
+			not_change_count = 0
+		elif not change(Q, Q2, env):
+			not_change_count += 1
+			if(not_change_count == change_no):
+				break
+		else:
+			not_change_count = 0
+
+	print ('Exceed: %d/%d' % (exceed, episode))
+	policy =  [[max(Q[i][j]) for i in range(grid_size)] for j in range(grid_size)]
+	plt.figure(1)
+	plt.clf()
+	plt.imshow(policy, interpolation='none', cmap='gray')
+	plt.savefig("greedy_max/policies/source_policy%d.png" % task_num)
+	return [Q, step]
+
+def EvalTask(Q, task, tau = 2000, epsilon = 0.3, alpha = 0.6, discount = 0.9):
+	# print (task[:-1])
+	# print (task[-1])
+	repeat_no = 3
+	env = Maze(len(Q), task[:-1], task[-1])
+	env.draw()
+	num_actions = env.num_actions
+	step = 0
+	tot_reward = 0
+	for i in range(repeat_no):
+		while(step < tau):
+			env.reset()
+			game_over = False
+			max_iter = 50
+			itr = 0
+			while not (game_over or itr > max_iter):
+				itr += 1
+				curr_state = env.state()
+				if np.random.rand() <= epsilon:
+					action = np.random.randint(0, num_actions)
+				else:
+					action = np.argmax(Q[curr_state[0]][curr_state[1]])
+				next_state, reward, game_over = env.act(action)
+				tot_reward += reward
+				step += 1
+				# Q-learning update
+				Q[curr_state[0]][curr_state[1]][action] = Q[curr_state[0]][curr_state[1]][action] + alpha*(reward + discount*max(Q[next_state[0]][next_state[1]]) - Q[curr_state[0]][curr_state[1]][action])
+			# if(itr > max_iter):
+			# 	print ('maximum steps exceeded, starting new episode.')
+	# print (tot_reward)
+	return [tot_reward/repeat_no, step/repeat_no]
+
+def findTask(Q, subtasks):
+	reward_list = []
+	max_reward = 0
+	tot_step = 0
+	# print (subtasks)
+	for task in subtasks:
+		Q2 = copy.deepcopy(Q)
+		[reward, step] = EvalTask(Q2, task)
+		tot_step += step
+		reward_list.append(reward)
+		if(reward > max_reward):
+			max_reward = reward
+			retTask = task
+	print (reward_list)
+	return [retTask, tot_step]
+
+if __name__ == "__main__":
+
+	grid_size = 7
+
+	# subtasks = [[[ 0,0 ] , [ 1,1 ] , [ 1,2 ] , [ 1,3 ] , [ 1,4 ] , [ 1,5 ] , [ 1,0 ]], [[ 0,0 ] , [ 1,1 ] , [ 1,2 ] , [ 1,3 ] , [ 1,4 ] , [ 1,5 ] , [ 1,0 ] , [ 2,0 ] , [ 3,0 ] , [ 4,0 ]], [[ 3,6 ] , [ 4,6 ] , [ 5,6 ] , [ 6,6 ]], [[ 3,6 ] , [ 4,6 ] , [ 5,6 ] , [ 6,6 ] , [ 6,5 ] , [ 6,4 ] , [ 6,3 ] , [ 6,2 ]], [[ 0,0 ] , [ 1,1 ] , [ 1,2 ] , [ 1,3 ] , [ 1,4 ] , [ 1,5 ] , [ 1,0 ] , [ 2,0 ] , [ 3,0 ] , [ 4,0 ] , [ 3,6 ] , [ 4,6 ] , [ 5,6 ] , [ 6,6 ] , [ 6,5 ] , [ 6,4 ] , [ 6,3 ] , [ 6,2 ] , [ 4,1 ] , [ 5,2 ] , [ 4,2 ]]]
+	subtasks = [[[ 0,0 ] , [ 1,1 ] , [ 1,2 ] , [ 1,3 ] , [ 1,4 ] , [ 1,5 ] , [ 1,0 ]], [[ 0,0 ] , [ 1,0 ] , [ 2,0 ] , [ 3,0 ] , [ 1,1 ] , [ 1,2 ] , [ 1,3 ] , [ 1,4 ] , [ 1,5 ] , [ 4,0 ]], [[ 6,5 ] , [ 6,4 ] , [ 6,3 ] , [ 6,2 ]], [[ 0,0 ] , [ 1,1 ] , [ 1,2 ] , [ 1,3 ] , [ 1,4 ] , [ 1,5 ] , [ 1,0 ] , [ 2,0 ] , [ 3,0 ] , [ 4,0 ] , [ 6,5 ] , [ 6,4 ] , [ 6,3 ] , [ 6,2 ] , [ 4,1 ] , [ 5,2 ] , [ 4,2 ]]]
+	# random.shuffle(subtasks)
+	# target_task = [[ 0,0 ] , [ 1,1 ] , [ 1,2 ] , [ 1,3 ] , [ 1,4 ] , [ 1,5 ] , [ 1,0 ] , [ 2,0 ] , [ 3,0 ] , [ 4,0 ] , [ 3,6 ] , [ 4,6 ] , [ 5,6 ] , [ 6,6 ] , [ 6,5 ] , [ 6,4 ] , [ 6,3 ] , [ 6,2 ] , [ 4,1 ] , [ 5,2 ] , [ 4,2 ] , [ 4,3 ] , [ 4,4 ]]
+	target_task = [[ 0,0 ] , [ 1,1 ] , [ 1,2 ] , [ 1,3 ] , [ 1,4 ] , [ 1,5 ] , [ 1,0 ] , [ 2,0 ] , [ 3,0 ] , [ 4,0 ] , [ 6,5 ] , [ 6,4 ] , [ 6,3 ] , [ 6,2 ] , [ 4,1 ] , [ 5,2 ] , [ 4,2 ] , [ 4,3 ] , [ 4,4 ]]
+
+	Q = [[[0,0,0,0] for i in range(grid_size)] for j in range(grid_size)]
+
+	tot_step = 0
+	task_num = 0
+	# [t, s] = EvalTask(Q, subtasks[1])
+	# print (t,s)
+	while len(subtasks) != 0:
+		task_num += 1
+		[task, tstep] = findTask(Q, subtasks)
+		tot_step += tstep
+		[Q, step] = learnTask(task_num, Q, task)
+		tot_step += step
+		print(tstep, step)
+		subtasks.remove(task)
