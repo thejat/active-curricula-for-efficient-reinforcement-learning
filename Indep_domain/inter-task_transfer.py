c128eba130439927b936ac1839cb331d46ec869e
Vikas Jain <vikasj@cse.iitk.ac.in>
2016-06-27 18:23:09 +0530

    pushing

--- Indep_domain/inter-task_transfer.py
+++ Indep_domain/inter-task_transfer.py
@@ -22,7 +22,6 @@ def learn_source(task_num, s_env, epsilon = 0.3, alpha = 0.6, discount = 0.9):
 	grid_size = s_env.grid_size
 	Q = [[[0,0,0,0] for i in range(grid_size)] for j in range(grid_size)]
 	num_actions = s_env.num_actions
-
 	## Learning source task
 	step = 0
 	episode = 0
@@ -42,7 +41,10 @@ def learn_source(task_num, s_env, epsilon = 0.3, alpha = 0.6, discount = 0.9):
 			if np.random.rand() <= epsilon:
 				action = np.random.randint(0, num_actions)
 			else:
-				action = np.argmax(Q[curr_state[0]][curr_state[1]])
+				if(max(Q[curr_state[0]][curr_state[1]]) == min(Q[curr_state[0]][curr_state[1]])):
+					action = -1#np.random.randint(0, num_actions)
+				else:
+					action = np.argmax(Q[curr_state[0]][curr_state[1]])
 			next_state, reward, game_over = s_env.act(action)
 			step += 1
 			# Q-learning update
@@ -56,8 +58,8 @@ def learn_source(task_num, s_env, epsilon = 0.3, alpha = 0.6, discount = 0.9):
 				break
 		else:
 			not_change_count = 0
-
-	print ('Exceed: %d/%d' % (exceed, episode))
+	# print ('Exceed: %d/%d' % (exceed, episode))
+	print (step)
 	policy =  [[max(Q[i][j]) for i in range(grid_size)] for j in range(grid_size)]
 	plt.figure(1)
 	plt.clf()
@@ -66,55 +68,58 @@ def learn_source(task_num, s_env, epsilon = 0.3, alpha = 0.6, discount = 0.9):
 	return Q
 
 par = 0
-def Transfer2(Q, f_env, epsilon = 0.1, alpha = 1, discount = 0.9): 
-	## Learning final task
-	num_actions = f_env.num_actions
-	step = 0
-	tot_reward = 0
-	episode = 0
-	exceed = 0
-	not_change_count = 0
-	change_no = 10
-	while True:
-		f_env.reset()
-		game_over = False
-		max_iter = 100
-		itr = 0
-		episode += 1
-		Q2 = copy.deepcopy(Q)
-		while not (game_over or itr > max_iter):
-			itr += 1
-			curr_state = f_env.state()
-			if np.random.rand() <= epsilon:
-				action = np.random.randint(0, num_actions)
-			else:
-				action = np.argmax(Q[curr_state[0]][curr_state[1]])
-			next_state, reward, game_over = f_env.act(action)
-			tot_reward += reward
-			step += 1
-			# Q-learning update
-			Q[curr_state[0]][curr_state[1]][action] = Q[curr_state[0]][curr_state[1]][action] + alpha*(reward + discount*max(Q[next_state[0]][next_state[1]]) - Q[curr_state[0]][curr_state[1]][action])
-		if (itr > max_iter):
-			exceed += 1
-			not_change_count = 0
-		elif not change(Q, Q2, s_env):
-			not_change_count += 1
-			if(not_change_count == change_no):
-				break
-		else:
-			not_change_count = 0
+# def Transfer2(Q, f_env, epsilon = 0.1, alpha = 1, discount = 0.9): 
+# 	## Learning final task
+# 	num_actions = f_env.num_actions
+# 	step = 0
+# 	tot_reward = 0
+# 	episode = 0
+# 	exceed = 0
+# 	not_change_count = 0
+# 	change_no = 10
+# 	while True:
+# 		f_env.reset()
+# 		game_over = False
+# 		max_iter = 100
+# 		itr = 0
+# 		episode += 1
+# 		Q2 = copy.deepcopy(Q)
+# 		while not (game_over or itr > max_iter):
+# 			itr += 1
+# 			curr_state = f_env.state()
+# 			if np.random.rand() <= epsilon:
+# 				action = np.random.randint(0, num_actions)
+# 			else:
+# 				if(max(Q[curr_state[0]][curr_state[1]]) == min(Q[curr_state[0]][curr_state[1]])):
+# 					action = -1#np.random.randint(0, num_actions)
+# 				else:
+# 					action = np.argmax(Q[curr_state[0]][curr_state[1]])
+# 			next_state, reward, game_over = f_env.act(action)
+# 			tot_reward += reward
+# 			step += 1
+# 			# Q-learning update
+# 			Q[curr_state[0]][curr_state[1]][action] = Q[curr_state[0]][curr_state[1]][action] + alpha*(reward + discount*max(Q[next_state[0]][next_state[1]]) - Q[curr_state[0]][curr_state[1]][action])
+# 		if (itr > max_iter):
+# 			exceed += 1
+# 			not_change_count = 0
+# 		elif not change(Q, Q2, s_env):
+# 			not_change_count += 1
+# 			if(not_change_count == change_no):
+# 				break
+# 		else:
+# 			not_change_count = 0
 
-	global par
-	policy =  [[max(Q[i][j]) for i in range(len(Q))] for j in range(len(Q))]
-	plt.figure(1)
-	plt.clf()
-	plt.imshow(policy, interpolation='none', cmap='gray')
-	plt.savefig("inter/policies/target_policy%d.png" % par)
-	par += 1
-	print (tot_reward, step)
-	return 1.0/step
+# 	global par
+# 	policy =  [[max(Q[i][j]) for i in range(len(Q))] for j in range(len(Q))]
+# 	plt.figure(1)
+# 	plt.clf()
+# 	plt.imshow(policy, interpolation='none', cmap='gray')
+# 	plt.savefig("inter/policies/target_policy%d.png" % par)
+# 	par += 1
+# 	print (tot_reward, step)
+# 	return 1.0/step
 
-def Transfer(Q, f_env, f_step = 10000, epsilon = 0.3, alpha = 0.2, discount = 0.9):
+def Transfer(Q, f_env, f_step = 500, epsilon = 0.3, alpha = 1.0, discount = 0.9):
 
 	## Learning final task
 	num_actions = f_env.num_actions
@@ -123,7 +128,7 @@ def Transfer(Q, f_env, f_step = 10000, epsilon = 0.3, alpha = 0.2, discount = 0.
 	while(step < f_step):
 		f_env.reset()
 		game_over = False
-		max_iter = 100
+		max_iter = 50
 		itr = 0
 		while not (game_over or itr > max_iter):
 			itr += 1
@@ -131,15 +136,19 @@ def Transfer(Q, f_env, f_step = 10000, epsilon = 0.3, alpha = 0.2, discount = 0.
 			if np.random.rand() <= epsilon:
 				action = np.random.randint(0, num_actions)
 			else:
-				action = np.argmax(Q[curr_state[0]][curr_state[1]])
+				if(max(Q[curr_state[0]][curr_state[1]]) == min(Q[curr_state[0]][curr_state[1]])):
+					action = -1#np.random.randint(0, num_actions)
+				else:
+					if(max(Q[curr_state[0]][curr_state[1]]) == min(Q[curr_state[0]][curr_state[1]])):
+						action = -1#np.random.randint(0, num_actions)
+					else:
+						action = np.argmax(Q[curr_state[0]][curr_state[1]])
 			next_state, reward, game_over = f_env.act(action)
 			tot_reward += reward
 			step += 1
 			# Q-learning update
 			Q[curr_state[0]][curr_state[1]][action] = Q[curr_state[0]][curr_state[1]][action] + alpha*(reward + discount*max(Q[next_state[0]][next_state[1]]) - Q[curr_state[0]][curr_state[1]][action])
-		# if(itr > max_iter):
-		# 	print ('maximum steps exceeded, starting new episode.')
-	print (tot_reward)
+	# print (tot_reward)
 	return tot_reward
 
 if __name__ == "__main__":
